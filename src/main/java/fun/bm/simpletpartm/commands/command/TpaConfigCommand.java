5534f9b7123cc7fc972a7e2e8b157dcbcd461efa
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-12-23 06:32:18 +0800

    fix: fix config

--- src/main/java/fun/bm/simpletpartm/commands/command/TpaConfigCommand.java
+++ src/main/java/fun/bm/simpletpartm/commands/command/TpaConfigCommand.java
@@ -6,6 +6,10 @@ import fun.bm.simpletpartm.configs.ConfigManager;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
+import net.minecraft.util.Formatting;
+
+import java.util.Objects;
 
 public class TpaConfigCommand extends AbstractCommand {
     public TpaConfigCommand() {
@@ -38,7 +42,17 @@ public class TpaConfigCommand extends AbstractCommand {
                                                                     .argument("key", StringArgumentType.string())
                                                                     .suggests(
                                                                             (context, builder) -> {
-                                                                                ConfigManager.configfile.completeConfigPath(StringArgumentType.getString(context, "key")).forEach(builder::suggest);
+                                                                                String path;
+                                                                                try {
+                                                                                    path = StringArgumentType.getString(context, "key");
+                                                                                } catch (Exception e) {
+                                                                                    path = "";
+                                                                                }
+                                                                                int dotIndex = path.lastIndexOf(".");
+                                                                                builder = builder.createOffset(builder.getInput().lastIndexOf(' ') + dotIndex + 2);
+                                                                                for (String s : ConfigManager.configfile.completeConfigPath(path)) {
+                                                                                    builder.suggest(s.substring(path.lastIndexOf('.') + 1));
+                                                                                }
                                                                                 return builder.buildFuture();
                                                                             })
                                                                     .executes(
@@ -54,10 +68,33 @@ public class TpaConfigCommand extends AbstractCommand {
                                                                                     .argument("value", StringArgumentType.string())
                                                                                     .suggests(
                                                                                             (context, builder) -> {
-                                                                                                String[] suggestions = ConfigManager.configfile.getConfigSuggestions(StringArgumentType.getString(context, "key"));
-                                                                                                for (String suggestion : suggestions) {
-                                                                                                    builder.suggest(suggestion);
+                                                                                                String path;
+                                                                                                try {
+                                                                                                    path = StringArgumentType.getString(context, "key");
+                                                                                                } catch (Exception e) {
+                                                                                                    path = "";
                                                                                                 }
+                                                                                                if (!ConfigManager.configfile.getAllConfigPaths("").contains(path)) {
+                                                                                                    return builder
+                                                                                                            .suggest("<ERROR CONFIG>", Text.literal("This config path does not exist."))
+                                                                                                            .buildFuture();
+                                                                                                }
+                                                                                                Object value = ConfigManager.configfile.getConfigOrigin(path);
+                                                                                                String[] suggestions = ConfigManager.configfile.getConfigSuggestions(path);
+                                                                                                builder.suggest(value.toString(), Text.literal("Default value")
+                                                                                                        .styled(style -> style.withColor(TextColor.fromFormatting(Formatting.GRAY))));
+                                                                                                if (suggestions == null) {
+                                                                                                    if (value instanceof Boolean) {
+                                                                                                        builder.suggest(String.valueOf(!(Boolean) value));
+                                                                                                    }
+                                                                                                } else {
+                                                                                                    for (String s : suggestions) {
+                                                                                                        if (!Objects.equals(s, value.toString())) {
+                                                                                                            builder.suggest(s);
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+
                                                                                                 return builder.buildFuture();
                                                                                             }
                                                                                     )
